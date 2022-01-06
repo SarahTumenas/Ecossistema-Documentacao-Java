49e0474ca304b812b0add932c14234f0c92ea74e
SarahTumenas <sarahtumenas@yahoo.com.br>
2022-01-06 16:35:46 -0300

    estrutura de condição

--- src/main/java/introducao/MyFirstProgram.java
+++ src/main/java/introducao/MyFirstProgram.java
@@ -5,7 +5,8 @@ import introducao.base.Order;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        Order order = new Order("code1234");
+        Order order = new Order("code 1234", 110);
+
 
         System.out.println(order);
     }
--- src/main/java/introducao/base/Order.java
+++ src/main/java/introducao/base/Order.java
@@ -1,16 +1,41 @@
 package introducao.base;
 
+import java.math.BigDecimal;
+
 public class Order {
-    private final String code;
+    public final String code;
+    public final double totalValue ;
 
-    public Order (String code){
+
+    public Order(String code, double totalValue) {
         this.code = code;
+        this.totalValue = totalValue;
     }
 
-    @Override
-    public String toString() {
-        return "Order{" +
-                "code='" + code + '\'' +
-                '}';
+    public double calculateFee (){
+     if (this.totalValue > 100){
+         return this.totalValue * 0.99;
+     }else {
+         return this.totalValue;
+     }
     }
+
+   /* switch Case
+    private  final int totalValue;
+
+    public double calulateFee (){
+        switch (this.totalValue){
+            case 100:
+                return totalValue * 0.99;
+            case 200:
+                return totalValue * 1.99;
+            default:
+                return totalValue;
+        }
+    }
+    */
+
+
+
+
 }
