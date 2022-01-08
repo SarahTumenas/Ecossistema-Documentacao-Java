279ff47b933df6899e763bd883ac7c9226e3a59f
SarahTumenas <sarahtumenas@yahoo.com.br>
2022-01-08 14:25:31 -0300

    Estudos ITA

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Neste repositório será compartilhado exercícios de Java, servindo de revisão dos principais conceitos e aplicações da linguagem
 
 - Introdução : conceitos de palavras reservadas, declarações de classes, modificadores de acesso, Métodos, Estruturas de condição e de repetição, Javadoc e Tags Javadoc
+- Estudos ITA:
+    - Hands-On: Classes e Objetos
---
+++ src/main/java/ITA/Carros/HandsOnCalssesEObjetos/Principal.java
@@ -0,0 +1,34 @@
+package ITA.Carros.HandsOnCalssesEObjetos;
+
+public class Principal {
+    public static void main(String[] args) {
+        Carro c1 = new Carro();
+        c1.potencia = 10;
+        c1.nome = "Corcel";
+        c1.velocidade = 0;
+        c1.imprimir();
+        c1.acelerar();
+        c1.imprimir();
+        c1.acelerar();
+        c1.acelerar();
+        c1.acelerar();
+        c1.frear();
+        c1.imprimir();
+
+        Carro c2 = new Carro();
+        c2.potencia = 100;
+        c2.nome = "Ferrari";
+        c2.velocidade = 0;
+        c2.imprimir();
+        c2.acelerar();
+        c2.imprimir();
+        c2.acelerar();
+        c2.acelerar();
+        c2.acelerar();
+        c2.frear();
+        c2.imprimir();
+
+        System.out.println("Carros : " + c1.nome + " e " + c2.nome);
+
+    }
+}
