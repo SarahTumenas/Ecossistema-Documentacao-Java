093211df221712c3672f3308f44a5e7855eba582
SarahTumenas <sarahtumenas@yahoo.com.br>
2022-01-13 10:40:37 -0300

    Estrutura Básica

---
+++ src/main/java/introducao/base/OrientacaoObjetos/RodarAplicacao.java
@@ -0,0 +1,24 @@
+package introducao.base.OrientacaoObjetos;
+
+public class RodarAplicacao {
+    public static void main(String[] args) {
+
+        Carro carro1 = new Carro();
+
+        carro1.setCor("azul");
+        carro1.setModelo("BMW");
+        carro1.setCapacidadeTanque(60);
+
+        System.out.println(carro1.getModelo());
+        System.out.println(carro1.getCor());
+        System.out.println(carro1.getCapacidadeTanque());
+        System.out.println(carro1.totalValorTanque(5.89));
+
+        Carro carro2 = new Carro("preto", "Fiat", 55);
+        System.out.println(carro2.getModelo());
+        System.out.println(carro2.getCor());
+        System.out.println(carro2.getCapacidadeTanque());
+        System.out.println(carro2.totalValorTanque(4.99));
+
+    }
+}
