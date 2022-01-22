cdbf40a52ad016d89564960345548c77eb5d9b12
SarahTumenas <sarahtumenas@yahoo.com.br>
2022-01-22 13:28:38 -0300

    statics

--- src/main/java/introducao/base/inicio/caracteristicaslinguagem/statics/Programa.java
+++ src/main/java/introducao/base/inicio/caracteristicaslinguagem/statics/Programa.java
@@ -3,14 +3,14 @@ package introducao.base.inicio.caracteristicaslinguagem.statics;
 public class Programa {
     public static void main(String[] args) {
         final Cachorro pitbull = new Cachorro();
-        Cachorro.zoologia = "Bipede";
+        pitbull.zoologia = "Bipede";
 
         final Cachorro viraLatas = new Cachorro();
 
         System.out.println(pitbull.zoologia);
         System.out.println(viraLatas.zoologia);
 
-        String late = Cachorro.late();
+        Cachorro.late();
 
     }
 }
