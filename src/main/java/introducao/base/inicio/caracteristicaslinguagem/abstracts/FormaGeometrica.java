9fff9216850c108a0df9a470d2517afbe5c15bad
SarahTumenas <sarahtumenas@yahoo.com.br>
2022-01-22 12:50:37 -0300

    abstracts

---
+++ src/main/java/introducao/base/inicio/caracteristicaslinguagem/abstracts/FormaGeometrica.java
@@ -0,0 +1,11 @@
+package introducao.base.inicio.caracteristicaslinguagem.abstracts;
+
+public abstract class FormaGeometrica {
+
+    public abstract String nome();
+    public abstract Double area();
+
+    public String desenha(int x , int y) {
+        return "Desenhando nas coordenadas X= "+x+" Y= "+y;
+    }
+}
