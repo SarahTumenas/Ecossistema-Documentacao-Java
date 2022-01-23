248849fc4a74f80ba5e7dff49a5827539f415c19
SarahTumenas <sarahtumenas@yahoo.com.br>
2022-01-23 10:02:58 -0300

    Operadores

---
+++ src/main/java/introducao/base/inicio/caracteristicasliguagem2/operadores/Incremento.java
@@ -0,0 +1,15 @@
+package introducao.base.inicio.caracteristicasliguagem2.operadores;
+
+public class Incremento {
+    public static void main(String[] args) {
+
+        var numero = 1;
+
+        System.out.println(++numero);
+
+        var variavel = 10;
+
+        System.out.println(variavel--);
+        System.out.println(variavel);
+    }
+}
