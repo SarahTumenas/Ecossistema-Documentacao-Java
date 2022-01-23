f5134b2d7ebbebca5ccc303f205da19e0385971b
SarahTumenas <sarahtumenas@yahoo.com.br>
2022-01-23 10:30:09 -0300

    IntStream

--- src/main/java/introducao/base/inicio/caracteristicasliguagem2/fors/For.java
+++ src/main/java/introducao/base/inicio/caracteristicasliguagem2/fors/For.java
@@ -1,5 +1,7 @@
 package introducao.base.inicio.caracteristicasliguagem2.fors;
 
+import java.util.stream.IntStream;
+
 public class For {
     public static void main(String[] args) {
 
@@ -9,6 +11,14 @@ public class For {
 
         for (int x = 0; x <= 5; x++)
             System.out.println("X=" + x);
+
+        IntStream.of(1,2,3,4,5).forEach(n->{
+            System.out.println(n);
+        });
+
+        IntStream.range(0,3).forEach(n->{
+            System.out.println("Número = " +n);
+        });
 
     }
 }
