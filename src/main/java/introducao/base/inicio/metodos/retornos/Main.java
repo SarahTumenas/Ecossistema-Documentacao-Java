e5a58579e80a4726c245e82d41eb6723f3f52e21
SarahTumenas <sarahtumenas@yahoo.com.br>
2022-01-25 10:54:59 -0300

    Métodos - retornos

---
+++ src/main/java/introducao/base/inicio/metodos/retornos/Main.java
@@ -0,0 +1,19 @@
+package introducao.base.inicio.metodos.retornos;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Retornos
+        System.out.println("Exercício retornos");
+
+        double areaQuadrado = Quadrilatero.area(3);
+        System.out.println("Area do quadrado:" + areaQuadrado);
+
+        double areaRetangulo = Quadrilatero.area(5,5);
+        System.out.println("Area do retângulo:" + areaRetangulo);
+
+        double areaTrapezio = Quadrilatero.area(7,8,9);
+        System.out.println("Area do trapézio:" + areaTrapezio);
+
+    }
+}
