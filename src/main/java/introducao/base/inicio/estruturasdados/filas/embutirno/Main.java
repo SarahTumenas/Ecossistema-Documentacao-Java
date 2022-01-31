20b79497096051781db93350a886571af038536a
SarahTumenas <sarahtumenas@yahoo.com.br>
2022-01-31 12:43:59 -0300

    Filas- embutir Nó

---
+++ src/main/java/introducao/base/inicio/estruturasdados/filas/embutirno/Main.java
@@ -0,0 +1,23 @@
+package introducao.base.inicio.estruturasdados.filas.embutirno;
+
+public class Main {
+    public static void main(String[] args){
+
+        Fila minhaFila = new Fila();
+
+        minhaFila.enqueue("primeiro");
+        minhaFila.enqueue("segundo");
+        minhaFila.enqueue("terceiro");
+        minhaFila.enqueue("quarto");
+
+        System.out.println(minhaFila);
+
+        System.out.println(minhaFila.dequeue());
+
+        System.out.println(minhaFila);
+
+        minhaFila.enqueue("ultimo");
+
+        System.out.println(minhaFila);
+    }
+}
